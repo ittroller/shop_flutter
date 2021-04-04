c9d811913234d0a8b0cfdb5bbf573cf76c45ba92
Cao Kha Minh <ittroller8@gmail.com>
2021-04-04 23:09:01 +0700

    product manage screen

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -68,6 +68,27 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+
+    if (_items[productId].quanlity > 1) {
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+            id: existingCartItem.id,
+            title: existingCartItem.title,
+            quanlity: existingCartItem.quanlity - 1,
+            price: existingCartItem.price),
+      );
+    } else {
+      _items.remove(productId);
+    }
+
+    notifyListeners();
+  }
+
   void clear() {
     _items = {};
     notifyListeners();
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -56,6 +56,19 @@ class ProductItem extends StatelessWidget {
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              // Scaffold.of(context).openDrawer();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('123'),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).accentColor,
           ),
