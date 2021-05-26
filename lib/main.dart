a794e0f2971e6cb881a249e2918a968773639ee9
Cao Kha Minh <ittroller8@gmail.com>
2021-05-26 23:34:01 +0700

    auth screen 1

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
 
+import './providers/auth.dart';
 import './providers/cart.dart';
 import './providers/products.dart';
 import './providers/orders.dart';
@@ -22,6 +23,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
         ChangeNotifierProvider(
           create: (ctx) => Products(),
         ),
